cc5366cd9be02a4038331a259209b4d719d4e07d
bogdan-Kozlovsky <bogdankozlovski18@gmail.com>
2022-02-02 17:27:36 +0200

    refactoring 48

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,66 @@
-import React, {FC} from 'react';
-import {Header} from './component/Header/Header';
-import {Navigation} from './component/Navigation/Navigation';
-import cn from 'classnames'
-import {Profile} from './component/Profile/Profile';
-import Dialogs from './component/Dialogs/Dialogs';
-import {Route, Routes} from 'react-router-dom';
-import News from './component/News/News';
-import Music from './component/Music/Music';
-import Settings from './component/Settings/Settings';
-import {
-    GenericType,
-    RootStateType,
-} from "./redux/state";
+import React from 'react';
+import './App.css';
+import {Header} from "./components/Header/Header";
+import {Profile} from "./components/Profile/Profile";
+import {Routes, Route, HashRouter} from "react-router-dom";
+import {DialogsContainer} from './components/Dialogs/DialogsContainer';
+import {NavbarContainer} from "./components/Navbar/NavbarContainer";
 
-type AppType = {
-    state: RootStateType
-    dispatch: (action: GenericType) => void
-}
+export type AppTypeProps = {}
 
+const App: React.FC<AppTypeProps> = (props) => {
 
-export const App = ({...props}: AppType) => {
-    const state = props.state
     return (
-        <div>
-            <Header/>
-
-            <div className="wrapper app">
-                <Navigation/>
-                <div className="app__box">
+        <HashRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <NavbarContainer/>
+                <div className='app-wrapper-content'>
                     <Routes>
-                        <Route path={'/'}
-                               element={<Profile
-                                   posts={state.profilePage.posts}
-                                   newPostText={state.profilePage.newPostText}
-                                   dispatch={props.dispatch}
-                               />}/>
-                        <Route path={'/dialogs/*'} element={<Dialogs
-                            dialog={state.dialogsPage.dialog}
-                            message={state.dialogsPage.message}
-                            dispatch={props.dispatch}
-                            newMessageText={props.state.dialogsPage.newMessageText}
-                        />}/>
-                        <Route path={'/news'} element={<News/>}/>
-                        <Route path={'/music'} element={<Music/>}/>
-                        <Route path={'/settings'} element={<Settings/>}/>
+                        <Route path='/' element={<Profile/>}/>
+                        <Route path='/dialogs/*' element={<DialogsContainer/>}/>
+                        <Route path='/news' element={<h2>News</h2>}/>
+                        <Route path='/music' element={<h2>Music</h2>}/>
+                        <Route path='/settings' element={<h2>Settings</h2>}/>
                     </Routes>
                 </div>
             </div>
-        </div>
-
+        </HashRouter>
     );
-};
+}
+
+export default App;
+
+
+// export const App = ({...props}: AppType) => {
+//     const state = props.state
+//     return (
+//         <div>
+//             <Header/>
+//
+//             <div className="wrapper app">
+//                 <Navigation/>
+//                 <div className="app__box">
+//                     <Routes>
+//                         <Route path={'/'}
+//                                element={<Profile
+//                                    posts={state.profilePage.posts}
+//                                    newPostText={state.profilePage.newPostText}
+//                                    dispatch={props.dispatch}
+//                                />}/>
+//                         <Route path={'/dialogs/*'} element={<Dialogs
+//                             dialog={state.dialogsPage.dialog}
+//                             message={state.dialogsPage.message}
+//                             dispatch={props.dispatch}
+//                             newMessageText={props.state.dialogsPage.newMessageText}
+//                         />}/>
+//                         <Route path={'/news'} element={<News/>}/>
+//                         <Route path={'/music'} element={<Music/>}/>
+//                         <Route path={'/settings'} element={<Settings/>}/>
+//                     </Routes>
+//                 </div>
+//             </div>
+//         </div>
+//
+//     );
+// };
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import './index.scss'
-import './normalize.scss'
+import React from 'react';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import {store} from'./redux/redux-store'
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
-import {App} from "./App";
-import {store} from './redux/reduxStore'
-import {RootStateType,} from "./redux/state";
+import App from "./App";
+import { Provider } from 'react-redux';
 
-export let rerenderEntireTree = (state: RootStateType) => {
     ReactDOM.render(
-        <BrowserRouter>
-            <App state={state}
-                 dispatch={store.dispatch.bind(store)}
-            />
-        </BrowserRouter>, document.getElementById('root'))
-}
-rerenderEntireTree(store.getState())
+        <React.StrictMode>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
 
-store.subscribe(() => {
-    let state = store.getState()
-    rerenderEntireTree(state)
-})
 
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
